b072de14bcac949a6a158f0f8d55fc48139d5174
Tanya Hristova <tanche98@gmail.com>
2019-12-18 15:05:24 +0200

    Delete no2.hs

--- Haskell/exam/no2.hs
+++
@@ -1,8 +0,0 @@
-notProgression :: Int -> Int -> Int -> Bool
-notProgression a b c = b - a /= c - b
-
-a 1 = 1
-a 2 = 1
-a n = head [ x | x <- [1..], all (\k -> (notProgression (a (n - 2*k)) (a (n - k)) x)) [1..(n `div` 2)]]
-
-forestFire = map a [1..]
